30a1635c97d442d83da266c08a80a66a8dc0697d
Eugene Birondo <jbirondo@gmail.com>
2020-10-30 05:18:15 -0700

    Working on Bio menu

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { stack as Menu } from 'react-burger-menu'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedinIn, faGithub, faAngellist } from '@fortawesome/free-brands-svg-icons'
 import "./Bio.css"
 
 class Bio extends React.Component {
@@ -10,18 +12,23 @@ class Bio extends React.Component {
     render () {
           return (
             <Menu right>
-            <a className="menu-item" href="/">
-                Home
-            </a>
-            <a className="menu-item" href="/salads">
-                Salads
-            </a>
-            <a className="menu-item" href="/pizzas">
-                Pizzas
-            </a>
-            <a className="menu-item" href="/desserts">
-                Desserts
-            </a>
+                <img src="https://media-exp1.licdn.com/dms/image/C5603AQE1q7mCXD5Ksw/profile-displayphoto-shrink_200_200/0?e=1609372800&v=beta&t=SsLOR6HGjZre1PR3KF7JIZ0PiS9tSne46NPabaHTS1Y"></img>
+                <h3>Eugene Birondo</h3>
+                    <p>
+                        Software Developer
+                    </p>
+                    <p>
+                        San Francisco Bay Area 
+                    </p> 
+                    <p> 
+                        Ruby, Ruby on Rails, JavaScript, Python, React, Redux, Node.js, Docker, GraphQL, Linux, Unix
+                    </p>
+                <ul className="contactInfo">
+                    <li key="linkedin"><FontAwesomeIcon icon={faLinkedinIn}/></li>
+                    <li key="github"><FontAwesomeIcon icon={faGithub}/></li>
+                    <li key="angellist"><FontAwesomeIcon icon={faAngellist}/></li>
+                </ul>
+
             </Menu>
         );
     }
